d3bb7ce7c0b40cb07ff7a98e301d71aca93420b1
martorres-utn <51000566+martorres-utn@users.noreply.github.com>
2019-11-18 14:35:40 -0300

    Initial commit

---
+++ cmake-build-debug/CMakeFiles/tp_2.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/tp_2.dir/main.cpp.obj"
+  "tp_2.pdb"
+  "tp_2.exe"
+  "tp_2.exe.manifest"
+  "libtp_2.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/tp_2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()

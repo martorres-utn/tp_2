abae67d776f1c386900f2d9c8c1390c935d9a052
martorres-utn <51000566+martorres-utn@users.noreply.github.com>
2019-11-24 22:05:43 -0300

    Update main.cpp
    Cambios hechos por FLor que no pudo commitear

--- main.cpp
+++ main.cpp
@@ -2,12 +2,40 @@
 #include "DataTypes.h"
 #include <bits/stdc++.h>
 
-
-
+void CargaProdsaVec(Prods RegP, TVP Vec[], int& top);
+void BuscaProdenVec ();
+TNodo* InsertaNodo ();
+TInfo Elimina1erNodo ();
+void ImprimeVecLista ();
 
 int main() {
+    TVP VP[100];
+    Prods RP;
+    TInfo RV;
+    int tope = 0;
 
+    std::cout << "Ingrese un codigo de producto (0 para finalizar la ejecucion): ";
+    std::cin >> RP.CodP;
+
+    while (RP.CodP != 0){
+        std::cout << "Ingrese una descripcion: ";
+        std::cin >> RP.Desc;
+        std::cout << "Ingrese el precio unitario del producto: ";
+        std::cin >> RP.PU;
+        CargaProdsaVec(RP, VP, tope);
+    }
 
 
     return 0;
+}
+
+void CargaProdsaVec(Prods RegP, TVP Vec[100], int &top) {
+    int i = 0;
+    for (i=0; i < 100; i++) {
+        Vec[i].CodP = RegP.CodP;
+        Vec[i].PU = RegP.PU;
+        Vec[i].lista = NULL;
+        //Vec[i].Desc = RegP.Desc; (lo comento ya que no me lo permite hacer el programa)
+    }
+    return;
 }

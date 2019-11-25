35865396372094a9eb2f9f16d6e1118cda6b3759
martorres-utn <51000566+martorres-utn@users.noreply.github.com>
2019-11-25 19:53:56 -0300

    Agrego insertaNodo y constructores default para los struct

--- DataTypes.h
+++ DataTypes.h
@@ -6,25 +6,28 @@
 #define TP_2_DATATYPES_H
 struct tProducto
 {
+    tProducto() : CodP(0), Desc(""), PU(0.0f)
+    {
+    }
     int CodP;
     char Desc[30];
     float PU;
 };
 
-struct tVenta
-{
-    int CodP;
-    int CantV;
-};
-
 struct tNodo
 {
+    tNodo() : totalFacturado(0.0f), sig(nullptr)
+    {
+    }
     float totalFacturado;
     tNodo* sig;
 };
 
 struct tProductoVenta
 {
+    tProductoVenta() : lista(nullptr)
+    {
+    }
     tProducto info;
     tNodo* lista;
 };
--- main.cpp
+++ main.cpp
@@ -7,7 +7,7 @@ void CargaProdsaVec(tProductoVenta vecProductoVenta[], int &tope) {
     for (tope = 0; tope < 100; tope++) {
         tProducto nuevoProducto;// (salta como error)
 
-        std::cout << "Ingrese un codigo de producto (0 para finalizar la ejecucion): " << std::endl;
+        std::cout << "[Ingreso de productos] : ingrese un codigo de producto (0 para finalizar la ejecucion): " << std::endl;
         std::cin >> nuevoProducto.CodP;
 
         if(nuevoProducto.CodP > 0)
@@ -27,6 +27,45 @@ void CargaProdsaVec(tProductoVenta vecProductoVenta[], int &tope) {
     }
 }
 
+int BuscaProdEnVec(tProductoVenta vecProductoVenta[], int tope, int codigoProducto)
+{
+    for(int index = 0; index < tope; index++)
+    {
+        if(vecProductoVenta[index].info.CodP == codigoProducto)
+            return index;
+    }
+    return -1;
+}
+
+tNodo * InsertaNodo(tNodo * &lista, float totalFacturado)
+{
+    tNodo * nuevo = new tNodo();
+    nuevo->totalFacturado = totalFacturado;
+
+    if(lista == nullptr || (lista != nullptr && nuevo->totalFacturado < lista->totalFacturado))
+    {
+        nuevo->sig = lista;
+        lista = nuevo;
+    }
+    else
+    {
+        tNodo * ptr = lista->sig;
+        tNodo * ant = nullptr;
+
+        while(ptr != nullptr && (nuevo->totalFacturado >= ptr->totalFacturado))
+        {
+            ant = ptr;
+            ptr = ptr->sig;
+        }
+
+        nuevo->sig = ptr;
+        if(ant != nullptr)
+            ant->sig = nuevo;
+    }
+
+    return nuevo;
+}
+
 int main() {
     tProductoVenta vecProductoVenta[100];
     int tope = 0;
@@ -39,6 +78,33 @@ int main() {
         std::cout << "CodP: " << vecProductoVenta[index].info.CodP << std::endl;
         std::cout << "Descripcion: " << vecProductoVenta[index].info.Desc << std::endl;
         std::cout << "Precio Unitario: " << vecProductoVenta[index].info.PU << std::endl;
+    }
+
+
+    std::cout << "[Ingreso de ventas] : ingrese un codigo de producto 0 para finalizar la ejecucion." << std::endl;
+
+    int codigoProducto = -1;
+    int cantidadVentas = 0;
+
+    while(codigoProducto != 0)
+    {
+        std::cout << "Ingrese Codigo de Producto: " << std::endl;
+        std::cin >> codigoProducto;
+
+        int posProducto = BuscaProdEnVec(vecProductoVenta, tope, codigoProducto);
+        if(posProducto > -1)
+        {
+            std::cout << "Ingrese una cantidad de ventas: " << std::endl;
+            std::cin >> cantidadVentas;
+
+            //Producto encontrado
+            float totalFacturado = vecProductoVenta[posProducto].info.PU * cantidadVentas;
+            tNodo * nodoVenta = InsertaNodo(vecProductoVenta[posProducto].lista, totalFacturado);
+        }
+        else
+        {
+            std::cout << "Codigo de Producto invalido, ingrese un codigo existente " << std::endl;
+        }
     }
 
     return 0;
